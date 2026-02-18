f29b346221c10558cfaa007d1545b97adc2489e5
Damir <d_bokenov@kbtu.kz>
2026-02-18 19:53:43 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ Prac03/inheritance/multiple_inheritance.py
@@ -0,0 +1,14 @@
+class Fly:
+    def fly(self):
+        print("Flying")
+
+class Swim:
+    def swim(self):
+        print("Swimming")
+
+class Duck(Fly, Swim):
+    pass
+
+duck = Duck()
+duck.fly()
+duck.swim()
