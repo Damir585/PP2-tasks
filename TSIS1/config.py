61267b8982b0ec24933f73a02bf3ec2d2ecc3232
Damir <d_bokenov@kbtu.kz>
2026-04-27 19:48:33 +0500

    TSIS1: PhoneBook Extended

---
+++ TSIS1/config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "phonebook",
+    "user": "postgres",
+    "password": "Miko0808",
+    "port": 5432
+}
---
+++ TSIS1/connect.py
@@ -0,0 +1,11 @@
+﻿import pg8000.dbapi
+from config import DB_CONFIG
+
+def get_connection():
+    return pg8000.dbapi.connect(
+        host=DB_CONFIG['host'],
+        database=DB_CONFIG['database'],
+        user=DB_CONFIG['user'],
+        password=DB_CONFIG['password'],
+        port=DB_CONFIG['port']
+    )
